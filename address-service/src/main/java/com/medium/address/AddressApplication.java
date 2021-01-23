e242483ba8e0edae8486dac56d5442d18b13cfb9
Rafael Martins <rafael_as_martins@Rafaels-MBP.Home>
2021-01-23 14:10:21 +0000

    Adding spring-kafka dependency

--- address-service/src/main/java/com/medium/address/AddressApplication.java
+++ address-service/src/main/java/com/medium/address/AddressApplication.java
@@ -3,7 +3,9 @@ package com.medium.address;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Supplier;
 
 @EnableDiscoveryClient
 @SpringBootApplication
